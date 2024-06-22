6e8c7853b0c3782d65318478e3746193993fec08
Lurence Tablo <lurencetablo@gmail.com>
2024-06-22 12:05:14 +0800

    cool warm

--- FreeMeanSystemAPI/FreeMeanSystemAPI/Controllers/WeatherForecastController.cs
+++ FreeMeanSystemAPI/FreeMeanSystemAPI/Controllers/WeatherForecastController.cs
@@ -21,13 +21,40 @@ namespace FreeMeanSystemAPI.Controllers
         [HttpGet(Name = "GetWeatherForecast")]
         public IEnumerable<WeatherForecast> Get()
         {
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            //return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            //{
+            //    Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            //    TemperatureC = Random.Shared.Next(-20, 55),
+            //    Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            //})
+            //.ToArray();
+            return Enumerable.Range(1, 5).Select(index =>
             {
-                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                TemperatureC = Random.Shared.Next(-20, 55),
-                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+                int temperatureC = Random.Shared.Next(-20, 55);
+                return new WeatherForecast
+                {
+                    Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                    TemperatureC = temperatureC,
+                    //Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+                    Summary = weatherSummary(temperatureC)
+                };
             })
             .ToArray();
+        }
+
+        private string weatherSummary(int Temperature)
+        {
+            string summary = string.Empty;
+            if (Temperature >= 17)
+            {
+                summary = Summaries.Single(s => s == "Warm");
+            }
+            else
+            {
+                summary = Summaries.Single(s => s == "Cool");
+            }
+                
+            return summary;
         }
     }
 }
